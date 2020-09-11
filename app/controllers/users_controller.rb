c479299bb16fbb64ceeace26f594a33a840beb03
kawasaki03 <h.kawasaki03@gmail.com>
2020-09-11 15:04:32 +0900

    ユーザー編集機能の実装

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,22 @@
+class UsersController < ApplicationController
+
+ def edit
+ end 
+
+ def update
+ if current_user.update(params_user)
+  redirect_to root_path
+ else
+  render action: :edit
+ end
+
+
+ end
+
+private
+def params_user
+  params.require(:user).permit(:name,:email)
+end
+ 
+
+end
