4207bfb28245c714b51c72393495606d1c6578ab
kawasaki03 <h.kawasaki03@gmail.com>
2020-09-13 22:18:41 +0900

    メッセージ送信機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
   devise_for :users
   resources :users,only: [:edit,:update]
  root to: "rooms#index"
-resources :rooms,only: [:new,:create]
-resources :messages,only: :index
+resources :rooms,only: [:new,:create] do
+resources :messages,only: [:index,:create]
+end
 
 end
